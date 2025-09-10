58884c516df47485a66588812b86381fbcc41bd8
Noel Mangwarira <ncmangwarira@gmail.com>
2025-09-10 21:07:59 +0200

    add new project

---
+++ alx-movie-app/components/layouts/Button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <button className="px-4 py-2 bg-green-600 text-white rounded">Layout Button</button>;
+};
+
+export default Button;
--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,18 +1,8 @@
-import withPWAInit from "@ducanh2912/next-pwa";
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-
-const withPWA = withPWAInit({
-  dest: 'public'
-})
-
-const nextConfig = {
+const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  images: {
-    domains: ['m.media-amazon.com'],
-  },
 };
 
-export default withPWA({
-  ...nextConfig
-})
+export default nextConfig;
